11b8b068acfc4e298f13f18819e144702f414d13
Dan Liebgold <dan.liebgold@gmail.com>
2014-05-25 15:15:19 -0700

    make color definitions module local; simplify module syntax

--- colors.rkt
+++ colors.rkt
@@ -14,28 +14,47 @@
 (define-syntax (color-module-begin stx)
   (syntax-case stx ()
     [(_ name form ...)
-     (with-syntax ([colors-id
-                    ;; non-hygenic binding:
-                    (datum->syntax stx (syntax->datum #'name))]
+	 #'(#%module-begin
+		;; original forms:
+		form ...
+		;; put at end, so that it's expanded after all
+		;; other top-level definitions:
+		(define-colors name))]))
 
-				   )
-       #'(#%module-begin
-          ;; original forms:
-          form ...
-          ;; put at end, so that it's expanded after all
-          ;; other top-level definitions:
-          (define-colors colors-id)))]))
+;; (define-syntax (color-module-begin stx)
+;;   (syntax-case stx ()
+;;     [(_ name form ...)
+;;      (with-syntax ([colors-id
+;;                     ;; non-hygenic binding:
+;;                     (datum->syntax stx (syntax->datum #'name))]
+
+;; 				   )
+;;        #'(#%module-begin
+;;           ;; original forms:
+;;           form ...
+;;           ;; put at end, so that it's expanded after all
+;;           ;; other top-level definitions:
+;;           (define-colors colors-id)))]))
 
 (begin-for-syntax
  (define id-to-color (make-hasheq)))
 
-(define-syntax-rule (color-define color-id #:color color expr)
-  (begin
-    (begin-for-syntax
-     ;; Register in compile-time mapping
-     (hash-set! id-to-color 'color-id 'color))
-    ;; Otherwise, act like regular `define':
-    (define color-id expr)))
+;; (define-syntax-rule (color-define color-id #:color color expr)
+;;   (begin
+;;     (begin-for-syntax
+;;      ;; Register in compile-time mapping
+;;      (hash-set! id-to-color 'color-id 'color))
+;;     ;; Otherwise, act like regular `define':
+;;     (define color-id expr)))
+
+ (define-syntax (color-define stx)
+   (syntax-case stx ()
+     [(_ color-id #:color color expr)
+      (begin
+        ;; Register in compile-time mapping
+        (hash-set! id-to-color (syntax-e #'color-id) (syntax-e #'color))
+        ;; Otherwise, act like regular `define':
+        #'(define color-id expr))]))
 
 (define-syntax (define-colors stx)
   (syntax-case stx ()
